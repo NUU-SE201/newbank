d75c093ef1d21d0680e5c73be7b6c3a23f3b11a9
Mardon Hazratov <mardonbekhazratov@gmail.com>
2026-04-10 17:05:15 +0500

    add home page and booking application

---
+++ booking/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from .models import Booking
+
+# Create your views here.
+def index(request):
+    my_booking = request.session.get('my_booking', None)
+    bookings = Booking.objects.all()
+    return render(request,
+                  'booking/index.html',
+                  {'bookings': bookings,
+                   'my_booking': my_booking})
+
+def add(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        if name:
+            new_booking = Booking(name=name)
+            new_booking.save()
+            request.session['my_booking'] = new_booking.id
+        return HttpResponseRedirect(reverse('booking:index'))
+    return render(request, 'booking/add.html')
