14c13b037bb77152e7f9b8f8987c0626001304e7
durdona1404 <d.valiqulova@newuu.uz>
2026-04-12 00:04:45 +0500

    done

--- newbank/settings.py
+++ newbank/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'home',
+    'booking',
     'cards',
     'currency',
     'django.contrib.admin',
--- newbank/urls.py
+++ newbank/urls.py
@@ -19,6 +19,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('home.urls')),
+    path('booking/', include('booking.urls')),
     path('cards/', include('cards.urls')),
     path('currency/', include('currency.urls')),
 ]
